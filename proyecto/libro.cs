f9b5eae576e76e3b0e5434ce3408ab3881629a5e
Roand <roandmoreno123@gmail.com>
2026-02-01 03:08:20 -0400

    libro y usuario

--- proyecto/libro.cs
+++ proyecto/libro.cs
@@ -22,5 +22,43 @@ namespace proyecto
         //    - IMPORTANTE: Asigna 'Disponible = true'.
 
         // 3. Método 'MostrarInformacion()': Imprimir datos en consola.
+
+        //definicion de propiedades
+        public string ISBN { get; set; } //get y set permiten acceder y modificar la propiedad (datos (string))
+        public string Titulo { get; set; }
+        public string Autor { get; set; }            
+        public int AnioPublicacion { get; set; }
+        public bool Disponible { get; set; }
+
+        public Libro(string isbn, string titulo, string autor , int anio) //constructor
+        {
+            ISBN = isbn;
+            Titulo = titulo;
+            Autor = autor;
+            AnioPublicacion = anio;
+            Disponible = true;
+        }
+
+        
+        public void MostrarInformacion() //metodo para mostrar informacion del libro
+        {
+            
+            string estado = Disponible ? "Disponible" : "Prestado";
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("       DETALLE DEL LIBRO");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Título:   {Titulo}");
+            Console.WriteLine($"Autor:    {Autor}");
+            Console.WriteLine($"ISBN:     {ISBN}");
+            Console.WriteLine($"Año:      {AnioPublicacion}");
+            Console.WriteLine($"Estado:   {estado}");
+            Console.WriteLine("-----------------------------------");
+        }
+
+
     }
+
+
+
 }
--- proyecto/usuario.cs
+++ proyecto/usuario.cs
@@ -18,7 +18,31 @@ namespace proyecto
         // 2. Constructor que recibe los datos personales.
         //    - Asigna los datos.
         //    - IMPORTANTE: Inicializa 'LibrosPrestados = new List<Libro>();'
-
         // 3. Métodos 'MostrarInformacion()' y 'MostrarLibrosPrestados()'.
+
+        public string DNI { get; set; } //get y set permiten acceder y modificar la propiedad (datos (string))
+        public string NombreCompleto { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public Usuario(string dni, string nombrecompleto, string email, string telefono) //constructor 
+        {
+            DNI = dni;
+            NombreCompleto = nombrecompleto;
+            Email = email;
+            Telefono = telefono;
+        }
+
+            public void MostrarInformacion() //metodo para mostrar informacion del usuario
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("       DATOS DEL USUARIO");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Nombre:   {NombreCompleto}");
+            Console.WriteLine($"DNI:      {DNI}");
+            Console.WriteLine($"Email:    {Email}");
+            Console.WriteLine($"Teléfono: {Telefono}");
+            Console.WriteLine("-----------------------------------");
+        }
     }
 }
+
